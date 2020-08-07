2b586ac731aa82c98460336b01bff0ce84403eb5
Gouson <Gouson@qq.com>
2020-08-07 20:11:36 +0800

    优化css

--- gouson-ui-1/src/lib/Switch.vue
+++ gouson-ui-1/src/lib/Switch.vue
@@ -7,12 +7,12 @@
 <script>
 import { ref } from "vue";
 export default {
-    props:{
-        value:Boolean
-    },
-  setup(props,context) {
+  props: {
+    value: Boolean,
+  },
+  setup(props, context) {
     const toggle = () => {
-        context.emit('update:value',!props.value)
+      context.emit("update:value", !props.value);
     };
     return { toggle };
   },
@@ -28,27 +28,37 @@ button {
   background: grey;
   border-radius: $h/2;
   position: relative;
-}
-span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2/2;
-  transition:left 250ms;//当left发生改变 =>动画
-}
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2/2;
+    transition: left 250ms; //当left发生改变 =>动画
+  }
 
-button.checked {
-  background: blue;
-}
-
-button.checked > span {
-  left: calc(100% - #{$h2} - 2px);
-}
-
-button:focus{
+  &.checked {
+    background: #2890ff;
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+  &:focus {
     outline: none;
+  }
+
+  &:active {
+    > span {
+      width: $h2 + 4px;
+    }
+  }
+  &.checked:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
+    }
+  }
 }
 </style>
