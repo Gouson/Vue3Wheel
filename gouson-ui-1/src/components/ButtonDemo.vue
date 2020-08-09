ada7da7a6ae185f081f7844be33ca3e51ac8bbf6
Gouson <Gouson@qq.com>
2020-08-09 21:01:01 +0800

    创建button

--- gouson-ui-1/src/components/ButtonDemo.vue
+++ gouson-ui-1/src/components/ButtonDemo.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     <p>Button 的文档</p>
-
+    <h1>示例1</h1>
+    <div> <Button>你好</Button></div>
   </div>
 </template>
 
-<script>
+<script lang="ts"> 
+import Button from '../lib/Button.vue'
+export default {
+  components:{Button}
+}
 </script>
 <style>
 </style>
