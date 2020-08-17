951319f3968904aafccd2bbb6e2b5bd00cc049da
Gouson <Gouson@qq.com>
2020-08-17 16:41:52 +0800

    css 最小影响原则

--- gouson-ui-1/src/components/ButtonDemo.vue
+++ gouson-ui-1/src/components/ButtonDemo.vue
@@ -2,18 +2,14 @@
   <div>
     <p>Button 的文档</p>
     <h1>示例1</h1>
-    <div>
-      <Button>你好</Button>
-    </div>
-    <div>
-      <Button theme="button">你好</Button>
-    </div>
-    <div>
-      <Button theme="link">你好</Button>
-    </div>
-    <div>
-      <Button theme="text">你好</Button>
-    </div>
+
+    <Button>你好</Button>
+
+    <Button theme="button">你好</Button>
+
+    <Button theme="link">你好</Button>
+
+    <Button theme="text">你好</Button>
   </div>
 </template>
 
