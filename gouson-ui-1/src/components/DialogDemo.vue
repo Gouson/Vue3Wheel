d7349cb57fe6df3e5bad48cf6ecf32be11f3f021
Gouson <Gouson@qq.com>
2020-08-26 18:43:28 +0800

    1

--- gouson-ui-1/src/components/DialogDemo.vue
+++ gouson-ui-1/src/components/DialogDemo.vue
@@ -1,17 +1,33 @@
 <template>
 <div>
-    <p>Dialog的文档</p>
-    <Dialog />
+    <div>实例1</div>
+    <Button @click="toggle">Toggle</Button>
+    <Dialog v-model:visiable="x" />
 </div>
 </template>
 
 <script lang="ts">
-import Dialog from '../lib/Dialog.vue'
+import Dialog from "../lib/Dialog.vue";
+import Button from "../lib/Button.vue";
+import {
+    ref
+} from "vue";
 export default {
     components: {
-        Dialog
-    }
-}
+        Dialog,
+        Button,
+    },
+    setup() {
+        const x = ref(false);
+        const toggle = () => {
+            x.value = !x.value;
+        };
+        return {
+            x,
+            toggle,
+        };
+    },
+};
 </script>
 
 <style>
