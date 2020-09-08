1f402bec86e4f6629160e6e51350f156b806e40f
Gouson <gouson@qq.com>
2020-09-08 17:08:30 +0800

    'test'

--- gouson-ui-1/src/lib/Tabs.vue
+++ gouson-ui-1/src/lib/Tabs.vue
@@ -5,6 +5,9 @@
 </template>
 <script lang="ts">
 export default {
-    
+    setup(props,context){
+        console.log({...context.slots.default()[0]})
+         console.log({...context.slots.default()[1]})
+    }
 }
 </script>
