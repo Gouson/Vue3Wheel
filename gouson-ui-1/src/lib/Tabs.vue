dcce91b00de5f55e6e8bf81ec15afcd7b1e38bba
Gouson <gouson@qq.com>
2020-09-10 13:53:25 +0800

    下划线滑动

--- gouson-ui-1/src/lib/Tabs.vue
+++ gouson-ui-1/src/lib/Tabs.vue
@@ -1,37 +1,73 @@
 <template>
-<div class="gulu-tabs">
-  <div class="gulu-tabs-nav">
-    <div class="gulu-tabs-nav-item" @click="select(t)" :class="{selected:t===selected}" v-for="(t,index) in titles" :key="index">{{t}}</div>
+  <div class="gulu-tabs">
+    <div class="gulu-tabs-nav" ref="container">
+      <div
+        class="gulu-tabs-nav-item"
+        @click="select(t)"
+        :ref="el=>{if(el) navItems[index] =el }"
+        :class="{selected:t===selected}"
+        v-for="(t,index) in titles"
+        :key="index"
+      >{{t}}</div>
+      <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
+    </div>
+    <div class="gulu-tabs-content">
+      <component
+        class="gulu-tabs-content-item"
+        :class="{selected:c.props.title === selected}"
+        v-for="(c,index) in defaults"
+        :is="c"
+        :key="index"
+      />
+    </div>
   </div>
-  <div class="gulu-tabs-content">
-    <component class="gulu-tabs-content-item" :class="{selected:c.props.title === selected}" v-for="(c,index) in defaults" :is="c" :key="index" />
-  </div>
-</div>
 </template>
 <script lang="ts">
-import Tab from './Tab.vue';
+import { onMounted, onUpdated, ref } from "vue";
+import Tab from "./Tab.vue";
 export default {
-  props:{
-    selected:{
-      type:String
-    }
+  props: {
+    selected: {
+      type: String,
+    },
   },
   setup(props, context) {
+    const navItems = ref<HTMLDivElement[]>([]);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+    const x = () => {
+      const divs = navItems.value;
+      const result = divs.filter((div) =>
+        div.classList.contains("selected")
+      )[0];
+      const { width } = result.getBoundingClientRect();
+      indicator.value.style.width = width + "px";
+      const { left: left1 } = container.value.getBoundingClientRect();
+      const { left: left2 } = result.getBoundingClientRect();
+      const left = left2 - left1;
+      indicator.value.style.left = left + "px";
+    };
+    onMounted(x);
+    onUpdated(x);
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
-      if(tag.type!==Tab){
-        throw new Error('Tabs子标签必须是Tab')
+      if (tag.type !== Tab) {
+        throw new Error("Tabs子标签必须是Tab");
       }
     });
-   const titles= defaults.map(tag => {
-      return tag.props.title
-      
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
     });
-    const select=(title:string)=>{
-      context.emit('update:selected',title)
-    }
+    const select = (title: string) => {
+      context.emit("update:selected", title);
+    };
     return {
-      defaults,titles,select
+      defaults,
+      titles,
+      select,
+      navItems,
+      indicator,
+      container,
     };
   },
 };
@@ -45,6 +81,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -56,12 +93,21 @@ $border-color: #d9d9d9;
         color: $blue;
       }
     }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
+    }
   }
   &-content {
     padding: 8px 0;
-    &-item{
-      display:none;
-      &.selected{
+    &-item {
+      display: none;
+      &.selected {
         display: block;
       }
     }
