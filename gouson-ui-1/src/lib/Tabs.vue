04c3f280b0578752385e4ad377a150538c563172
Gouson <gouson@qq.com>
2020-09-14 15:16:51 +0800

    icon

--- gouson-ui-1/src/lib/Tabs.vue
+++ gouson-ui-1/src/lib/Tabs.vue
@@ -4,7 +4,7 @@
       <div
         class="gulu-tabs-nav-item"
         @click="select(t)"
-        :ref="el=>{if(el) navItems[index] =el }"
+        :ref="el=>{if(t=== selected) selectedItem =el }"
         :class="{selected:t===selected}"
         v-for="(t,index) in titles"
         :key="index"
@@ -23,7 +23,7 @@
   </div>
 </template>
 <script lang="ts">
-import { onMounted, onUpdated, ref } from "vue";
+import { onMounted, ref, watchEffect } from "vue";
 import Tab from "./Tab.vue";
 export default {
   props: {
@@ -32,23 +32,19 @@ export default {
     },
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([]);
+    const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
-    const x = () => {
-      const divs = navItems.value;
-      const result = divs.filter((div) =>
-        div.classList.contains("selected")
-      )[0];
-      const { width } = result.getBoundingClientRect();
-      indicator.value.style.width = width + "px";
-      const { left: left1 } = container.value.getBoundingClientRect();
-      const { left: left2 } = result.getBoundingClientRect();
-      const left = left2 - left1;
-      indicator.value.style.left = left + "px";
-    };
-    onMounted(x);
-    onUpdated(x);
+    onMounted(() => {
+      watchEffect(() => {
+        const { width } = selectedItem.value.getBoundingClientRect();
+        indicator.value.style.width = width + "px";
+        const { left: left1 } = container.value.getBoundingClientRect();
+        const { left: left2 } = selectedItem.value.getBoundingClientRect();
+        const left = left2 - left1;
+        indicator.value.style.left = left + "px";
+      });
+    });
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
@@ -65,7 +61,7 @@ export default {
       defaults,
       titles,
       select,
-      navItems,
+      selectedItem,
       indicator,
       container,
     };
--- gouson-ui-1/src/views/Home.vue
+++ gouson-ui-1/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <Topnav/>
+  <div class="topnavAndBanner">
+    <Topnav />
     <div class="banner">
       <h1>ui</h1>
       <h2>厉害ui</h2>
@@ -10,32 +10,60 @@
       </p>
     </div>
   </div>
+  <div class="features">
+    <svg class="icon">
+      <use xlink:href="#icon-vue" />
+    </svg>
+    <svg class="icon">
+      <use xlink:href="#icon-ts" />
+    </svg>
+    <svg class="icon">
+      <use xlink:href="#icon-light" />
+    </svg>
+  </div>
 </template>
 <script lang="ts">
-import Topnav from '../components/Topnav.vue'
+import Topnav from "../components/Topnav.vue";
 export default {
-    components:{Topnav}
-}
+  components: { Topnav },
+};
 </script>
 <style lang="scss" scoped>
+$green: #02bcb0;
+$border-radius: 4px;
+$color: #007974;
+.topnavAndBanner {
+  background: linear-gradient(
+    145deg,
+    rgba(227, 255, 253, 1) 0%,
+    rgba(183, 233, 230, 1) 100%
+  );
+}
+.features{
+  >svg{
+    width: 64px;
+    height: 64px;
+  }
+}
 .banner {
+  color: $color;
   padding: 100px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  background: lightgreen;
-  > .actions{
+  > .actions {
     padding: 8px 0;
     a {
-      margin:0 8px;
-      background: #fff;
+      margin: 0 8px;
+      background: $green;
+      color: #fff;
       display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+      padding: 8px 24px;
+      border-radius: $border-radius;
+      &:hover {
+        text-decoration: none;
+      }
     }
   }
 }
